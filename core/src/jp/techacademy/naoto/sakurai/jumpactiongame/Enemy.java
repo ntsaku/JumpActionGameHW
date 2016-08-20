2a4a2c0fdba67ceb839a00df2166ceed71ac072a
ntsaku <misc.sakurai@gmail.com>
2016-08-20 18:59:58 +0900

    for homework

---
+++ core/src/jp/techacademy/naoto/sakurai/jumpactiongame/Enemy.java
@@ -0,0 +1,39 @@
+package jp.techacademy.naoto.sakurai.jumpactiongame;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Enemy extends GameObject  {
+
+    // 速度
+    public static final float ENEMY_VELOCITY = 1.0f;
+
+    // 横幅、高さ
+    public static final float ENEMY_WIDTH = 0.8f;
+    public static final float ENEMY_HEIGHT = 0.8f;
+
+
+
+    public Enemy(Texture texture, int srcX, int srcY, int srcWidth, int srcHeight) {
+        super(texture, srcX, srcY, srcWidth, srcHeight);
+        setSize(ENEMY_WIDTH, ENEMY_HEIGHT);
+
+        velocity.x = ENEMY_VELOCITY;
+
+    }
+
+
+    public void update(float deltaTime) {
+        setX(getX() + velocity.x * deltaTime);
+
+        if (getX() < ENEMY_WIDTH / 2) {
+            velocity.x = -velocity.x;
+            setX(ENEMY_WIDTH / 2);
+        }
+        if (getX() > GameScreen.WORLD_WIDTH - ENEMY_WIDTH / 2) {
+            velocity.x = -velocity.x;
+            setX(GameScreen.WORLD_WIDTH - ENEMY_WIDTH / 2);
+        }
+    }
+}
